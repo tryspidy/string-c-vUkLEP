4b44545839da3cf6e1f0cba208553752fa5a3d92
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-10 22:24:34 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <string>
+std::begin		| returns an iterator to the beginning of a container 
+std::end		| returns an iterator to the end of a container 
+std::size		| returns the length of string
+std::to_string	| converts a number to string
+std::stoi		| converts a string to a signed integer
+std::getline 	| read data from an I/O stream into a string
+std::swap  		| specializes the std::swap algorithm
+std::empty 		| checks whether the container is empty
